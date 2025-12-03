4411e663f68d2cd20f6debbd559c2c0a3b0dc89a
Vikindor <vikindor@proton.me>
2025-12-03 16:44:15 +0300

    fix: update phone locator

--- src/test/java/io/github/vikindor/pages/components/FooterComponent.java
+++ src/test/java/io/github/vikindor/pages/components/FooterComponent.java
@@ -3,18 +3,17 @@ package io.github.vikindor.pages.components;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FooterComponent {
 
     private final SelenideElement footer = $("footer[class^=Footer_wrap]");
-    private final ElementsCollection phoneLink = footer.$$("[href*='tel']");
+    private final ElementsCollection phones = footer.$$("a[href^='tel:']");
     private final SelenideElement callBackButton = footer.$("button[class^=Button]");
 
     public FooterComponent shouldHavePhone(String phoneNumber) {
-        phoneLink.findBy(text(phoneNumber)).shouldBe(visible);
+        phones.findBy(attribute("href", "tel:" + phoneNumber)).shouldBe(visible);
         return this;
     }
 
--- src/test/java/io/github/vikindor/pages/components/HeaderComponent.java
+++ src/test/java/io/github/vikindor/pages/components/HeaderComponent.java
@@ -3,20 +3,19 @@ package io.github.vikindor.pages.components;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class HeaderComponent {
 
     private final SelenideElement header = $("header[class^=Header_wrap]");
-    private final ElementsCollection phone = header.$$("[href*='tel']");
+    private final ElementsCollection phones = header.$$("a[href^='tel:']");
     private final SelenideElement searchInput = $("#searchInput");
     private final SelenideElement searchSuggestions = $("[class^=Search_bottom]");
     private final SelenideElement popularItems = searchSuggestions.$("[class^=Search_popularCards]");
 
     public HeaderComponent shouldHavePhone(String phoneNumber) {
-        phone.findBy(text(phoneNumber)).shouldBe(visible);
+        phones.findBy(attribute("href", "tel:" + phoneNumber)).shouldBe(visible);
         return this;
     }
 
